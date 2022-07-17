28585f264fb3422c217162362c240d12ae4a765b
sameera222 <100690618+sameera222@users.noreply.github.com>
2022-07-17 04:05:42 -0700

    Add files via upload

---
+++ JavaScript Projects/Calculator/simpleCalculator.js
@@ -0,0 +1,18 @@
+//This function clear all the values
+function clearScreen(){
+    document.getElementById("result").value="";
+
+}
+
+//This function display Values
+function display(value){
+    document.getElementById("result").value += value;
+}
+
+//This function evaluates the expression and return result
+function calculate(){
+    let p = document.getElementById("result").value;
+    let q = eval(p);
+    document.getElementById("result").value=q;
+}
+
